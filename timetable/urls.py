5e86544893ef9712391a4c3453041e118ecc780c
1nary <naritoshi.2714.p3@gmail.com>
2023-02-17 05:56:10 +0900

    初期アクセスURLの変更

--- timetable/urls.py
+++ timetable/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.urls import include
 
 urlpatterns = [
-    path('timetable/admin/', admin.site.urls),
-    path('timetable/', include('create.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('create.urls')),
 ]
