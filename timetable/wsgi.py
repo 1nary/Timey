d82c35ff34b3695ec055f3a8df59aedf544037ea
1nary <naritoshi.2714.p3@gmail.com>
2023-02-17 06:38:00 +0900

    ファイル修正

--- timetable/wsgi.py
+++ timetable/wsgi.py
@@ -2,6 +2,6 @@ import os
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'timetable.settings')
 
 application = Cling(get_wsgi_application())
